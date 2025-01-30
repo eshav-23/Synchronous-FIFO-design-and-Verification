8d280ee5ae0deecb780ec0e2cf6cf914941912d9
eshav-23 <128998458+eshav-23@users.noreply.github.com>
2025-01-31 03:25:16 +0530

    interface.sv

--- interface.sv
+++ interface.sv
@@ -1,1 +1,32 @@
+//Fifo interface
 
+interface fifo_interface(input logic clk,rst);
+
+  logic [7:0]data;
+  logic [7:0]data_o;
+  logic empty;
+  logic full;
+  logic rd_en;
+  logic wr_en;
+  logic [3:0]fifo_cnt;
+  
+  clocking driver_cb @(posedge clk);
+    output data;
+    output rd_en,wr_en;
+    input full,empty;
+    input data_o;
+    input fifo_cnt;
+  endclocking
+  
+  clocking monitor_cb @(posedge clk);
+    input data;
+    input rd_en,wr_en;
+    input full,empty;
+    input data_o;
+    input fifo_cnt;
+  endclocking  
+  
+  modport DRIVER(clocking driver_cb,input clk,rst);
+  modport MONITOR(clocking monitor_cb,input clk,rst);
+  
+endinterface
